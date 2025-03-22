8c91fa6e8bc07abd1dac0ecc5fd6686131e52f12
Ivan Gemota <ivangemota23@gmail.com>
2025-03-22 13:08:11 +0800

    chore: added intellectual property policy page

---
+++ src/app/pages/intellectual-property-policy/page.tsx
@@ -0,0 +1,193 @@
+export default function IntellectualPropertyPolicy() {
+	return (
+		<div className="flex flex-col gap-10">
+			<h1 className="text-5xl">Intellectual Property Policy</h1>
+
+			<div className="flex flex-col gap-6 text-gray-600 text-sm">
+				<div className="flex flex-col gap-4">
+					<span className="font-bold">Last Updated: February 4, 2025</span>
+					<span className="font-regular leading-7">
+						<span className="font-bold">IMPORTANT NOTICE:</span> BY ACCESSING,
+						UTILIZING, OR ENGAGING WITH THE SERVICES OF MED EXCUSE, YOU
+						IRREVOCABLY ASSENT TO THE INTELLECTUAL PROPERTY TERMS HEREIN. ANY
+						DISAGREEMENT WITH THESE TERMS NECESSITATES IMMEDIATE CESSATION OF
+						SERVICE USAGE.{" "}
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						1. OWNERSHIP OF INTELLECTUAL PROPERTY
+					</span>
+					<span className="font-regular leading-7">
+						<span className="font-bold">COMPANY NAME</span>
+						{` (hereinafter referred to as "Company," "We," "Us," "Our"), a legally registered entity under the jurisdiction of Hong Kong, retains exclusive ownership over all content, trademarks, domain names, proprietary methodologies, textual compositions, graphical elements, software applications, and all derivative works made available through MedExcuse.com (hereinafter referred to as "Website").
+
+`}
+					</span>
+					<span className="leading-7">
+						{`UNDER NO CIRCUMSTANCES SHALL USERS OBTAIN ANY EXPRESS OR IMPLIED RIGHTS, TITLE, LICENSE, OR INTEREST IN ANY INTELLECTUAL PROPERTY ASSOCIATED WITH THE WEBSITE, EXCEPT AS EXPRESSLY PERMITTED UNDER THESE TERMS. ANY UNAUTHORIZED REPRODUCTION, DISTRIBUTION, MODIFICATION, OR EXPLOITATION OF OUR INTELLECTUAL PROPERTY IS STRICTLY PROHIBITED AND MAY RESULT IN CIVIL OR CRIMINAL LIABILITY UNDER APPLICABLE LAWS.`}
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						2. LIMITED LICENSE GRANT
+					</span>
+					<span className="leading-7">
+						USERS ARE HEREBY GRANTED A REVOCABLE, NON-EXCLUSIVE,
+						NON-TRANSFERABLE, AND STRICTLY LIMITED RIGHT TO ACCESS AND UTILIZE
+						THE WEBSITE AND ITS CONTENTS SOLELY FOR PERSONAL, NON-COMMERCIAL
+						PURPOSES. ANY ATTEMPT TO SUBLICENSE, DISSEMINATE, REPURPOSE, OR
+						CREATE DERIVATIVE WORKS FROM ANY CONTENT HOSTED ON THE WEBSITE,
+						WHETHER FOR PROFIT OR OTHERWISE, SHALL CONSTITUTE A MATERIAL BREACH
+						OF THIS POLICY AND MAY RESULT IN LEGAL PROCEEDINGS, INCLUDING BUT
+						NOT LIMITED TO CLAIMS FOR INJUNCTIVE RELIEF AND MONETARY DAMAGES.
+					</span>
+					<span className="leading-7">
+						ANY ATTEMPT TO SUBLICENSE, DISSEMINATE, REPURPOSE, OR CREATE
+						DERIVATIVE WORKS FROM ANY CONTENT HOSTED ON THE WEBSITE, WHETHER FOR
+						PROFIT OR OTHERWISE, SHALL CONSTITUTE A MATERIAL BREACH OF THIS
+						POLICY AND MAY RESULT IN LEGAL PROCEEDINGS, INCLUDING BUT NOT
+						LIMITED TO CLAIMS FOR INJUNCTIVE RELIEF AND MONETARY DAMAGES.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						3. TRADEMARKS AND BRAND IDENTITY
+					</span>
+					<span className="leading-7">
+						ALL TRADEMARKS, LOGOS, SERVICE MARKS, TRADE DRESS, AND BRANDING
+						ELEMENTS DISPLAYED ON THE WEBSITE ARE THE EXCLUSIVE PROPERTY OF THE
+						COMPANY AND SHALL NOT BE UTILIZED IN ANY MANNER THAT COULD CAUSE
+						CONFUSION OR DECEPTION IN CONNECTION WITH ANY THIRD-PARTY ENTITY,
+						PRODUCT, OR SERVICE.
+					</span>
+					<span className="leading-7">
+						ANY UNAUTHORIZED USE, INCLUDING BUT NOT LIMITED TO REPRODUCTION,
+						COUNTERFEITING, OR MODIFICATION OF COMPANY TRADEMARKS, SHALL BE MET
+						WITH STRICT LEGAL RECOURSE.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						4. USER-GENERATED CONTENT AND SUBMISSIONS
+					</span>
+					<span className="leading-7">
+						IN THE EVENT THAT USERS SUBMIT REVIEWS, FEEDBACK, OR OTHER CONTENT
+						THROUGH THE WEBSITE OR ASSOCIATED PLATFORMS, SUCH SUBMISSIONS SHALL
+						BE DEEMED TO BE PROVIDED ON A ROYALTY-FREE, PERPETUAL, IRREVOCABLE,
+						AND WORLDWIDE LICENSED BASIS TO THE COMPANY FOR ANY LAWFUL PURPOSE,
+						INCLUDING BUT NOT LIMITED TO REPRODUCTION, MODIFICATION,
+						DISTRIBUTION, OR COMMERCIAL UTILIZATION.
+					</span>
+					<span className="leading-7">
+						USERS HEREBY WAIVE ANY MORAL RIGHTS OR CLAIMS ASSOCIATED WITH SUCH
+						SUBMISSIONS, INCLUDING BUT NOT LIMITED TO ATTRIBUTION, EDITORIAL
+						CONTROL, OR FUTURE COMPENSATION.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						5. COPYRIGHT INFRINGEMENT AND DMCA POLICY
+					</span>
+
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							THE COMPANY RESPECTS INTELLECTUAL PROPERTY RIGHTS AND COMPLIES
+							WITH THE DIGITAL MILLENNIUM COPYRIGHT ACT (DMCA). IF YOU BELIEVE
+							THAT ANY CONTENT AVAILABLE THROUGH THE WEBSITE INFRINGES UPON YOUR
+							COPYRIGHT, YOU MAY SUBMIT A NOTICE TO OUR DESIGNATED COPYRIGHT
+							AGENT AT SUPPORT@MEDEXCUSE.COM WITH THE FOLLOWING DETAILS:
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li>
+								A PHYSICAL OR ELECTRONIC SIGNATURE OF THE COPYRIGHT OWNER OR
+								AUTHORIZED REPRESENTATIVE;
+							</li>
+							<li>
+								IDENTIFICATION OF THE COPYRIGHTED WORK ALLEGEDLY INFRINGED;
+							</li>
+							<li>
+								IDENTIFICATION OF THE INFRINGING MATERIAL AND ITS LOCATION ON
+								THE WEBSITE;
+							</li>
+							<li>CONTACT INFORMATION OF THE COMPLAINANT;</li>
+							<li>
+								A SWORN STATEMENT UNDER PENALTY OF PERJURY AFFIRMING GOOD FAITH
+								BELIEF IN THE INFRINGEMENT CLAIM.
+							</li>
+						</ul>
+						<span>
+							COUNTER-NOTIFICATIONS MAY BE FILED IN RESPONSE TO A DMCA NOTICE
+							PURSUANT TO APPLICABLE STATUTORY REQUIREMENTS.
+						</span>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						6. UNAUTHORIZED USE AND ENFORCEMENT
+					</span>
+					<span className="leading-7">
+						ANY UNAUTHORIZED USAGE, REPRODUCTION, OR COMMERCIALIZATION OF OUR
+						INTELLECTUAL PROPERTY MAY RESULT IN IMMEDIATE TERMINATION OF USER
+						ACCESS, INJUNCTIVE RELIEF, AND CLAIMS FOR DAMAGES. USERS HEREBY
+						CONSENT TO THE EXERCISE OF ENFORCEMENT MEASURES TO THE MAXIMUM
+						EXTENT PERMISSIBLE UNDER LAW.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						7. MODIFICATIONS TO INTELLECTUAL PROPERTY POLICY
+					</span>
+					<span className="leading-7">
+						THE COMPANY RESERVES THE UNEQUIVOCAL RIGHT TO AMEND, RESCIND, OR
+						REVISE THIS POLICY WITHOUT PRIOR NOTICE. USERS SHALL BE SOLELY
+						RESPONSIBLE FOR PERIODICALLY REVIEWING THE POLICY TO ENSURE
+						COMPLIANCE.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						8. CONTACT INFORMATION
+					</span>
+
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							FOR INTELLECTUAL PROPERTY INQUIRIES OR REPORTING INFRINGEMENTS,
+							USERS MAY CONTACT:{" "}
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li className="font-bold">Company Name: COMPANY NAME</li>
+							<li className="font-bold">Registered Address: COMPANY ADDRESS</li>
+							<li className="font-bold">
+								Company Registration Number: COMPANY NUMBER
+							</li>
+							<li>
+								<span className="font-bold">Email: </span>
+								support@medexcuse.com
+							</li>
+							<li>
+								<span className="font-bold">Website: </span>
+								https://medexcuse.com
+							</li>
+						</ul>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+
+				<span className="leading-7">
+					BY ENGAGING WITH OUR SERVICES, YOU HEREBY WARRANT THAT YOU HAVE READ,
+					UNDERSTOOD, AND IRREVOCABLY ACCEPTED THE TERMS OF THIS INTELLECTUAL
+					PROPERTY POLICY IN ITS ENTIRETY{" "}
+				</span>
+			</div>
+		</div>
+	);
+}

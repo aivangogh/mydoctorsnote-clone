00eb428dc790701019c1b9febe9ac76232c30e81
Ivan Gemota <ivangemota23@gmail.com>
2025-03-22 13:33:49 +0800

    chore: added indemnification policy page

---
+++ src/app/pages/indemnification-policy/page.tsx
@@ -0,0 +1,174 @@
+export default function IndemnificationPolicy() {
+	return (
+		<div className="flex flex-col gap-10">
+			<h1 className="text-5xl">Indemnification Policy</h1>
+
+			<div className="flex flex-col gap-6 text-gray-600 text-sm">
+				<div className="flex flex-col gap-4">
+					<span className="font-bold">Last Updated: February 4, 2025</span>
+					<span className="font-regular leading-7">
+						<span className="font-bold">IMPORTANT NOTICE:</span>BY ACCESSING,
+						UTILIZING, OR ENGAGING WITH THE SERVICES OF MED EXCUSE, USERS
+						EXPRESSLY CONSENT TO THE INDEMNIFICATION OBLIGATIONS HEREIN. ANY
+						DISAGREEMENT NECESSITATES IMMEDIATE DISCONTINUATION OF SERVICE
+						USAGE.{" "}
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						1. GENERAL INDEMNIFICATION REQUIREMENT
+					</span>
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							USERS HEREBY AGREE TO DEFEND, INDEMNIFY, AND HOLD HARMLESS{" "}
+							<span className="font-bold">COMPANY NAME</span>{" "}
+							{`(HEREINAFTER
+							REFERRED TO AS "COMPANY," "WE," "US," "OUR"), ITS OFFICERS,
+							DIRECTORS, EMPLOYEES, AGENTS, AFFILIATES, SUCCESSORS, AND ASSIGNS
+							FROM AND AGAINST ANY AND ALL CLAIMS, LIABILITIES, LOSSES, DAMAGES,
+							COSTS, AND EXPENSES, INCLUDING WITHOUT LIMITATION, REASONABLE
+							ATTORNEYS' FEES AND COURT COSTS, ARISING OUT OF, RELATING TO, OR
+	`}{" "}
+							RESULTING FROM:
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li>
+								ANY BREACH OF THIS POLICY OR OTHER APPLICABLE TERMS BY THE USER;
+							</li>
+							<li>
+								USER-GENERATED CONTENT, SUBMISSIONS, OR COMMUNICATIONS THAT
+								INFRINGE UPON ANY THIRD-PARTY RIGHTS;
+							</li>
+							<li>
+								ANY VIOLATION OF APPLICABLE LAWS, STATUTES, OR REGULATORY
+								FRAMEWORKS;
+							</li>
+							<li>
+								THE UTILIZATION, MISUSE, OR INTERPRETATION OF THE COMPANY’S
+								SERVICES OR DOCUMENT TEMPLATES;
+							</li>
+							<li>
+                {`ANY DISPUTE, CLAIM, OR LEGAL PROCEEDING INITIATED BY A THIRD
+								PARTY ARISING FROM THE USER'S INTERACTION WITH OR RELIANCE UPON
+								THE SERVICES.`}
+							</li>
+						</ul>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						2. SCOPE OF INDEMNIFICATION
+					</span>
+					<span className="leading-7">
+						THE INDEMNIFICATION OBLIGATIONS HEREIN SHALL APPLY REGARDLESS OF THE
+						NATURE OF THE CLAIM, WHETHER SOUNDING IN CONTRACT, TORT, STRICT
+						LIABILITY, OR ANY OTHER LEGAL THEORY. USERS EXPRESSLY WAIVE ANY
+						RIGHT TO CHALLENGE THE ENFORCEMENT OF THIS INDEMNIFICATION
+						OBLIGATION ON GROUNDS OF EQUITY, FAIRNESS, OR ALLEGED LACK OF
+						NOTICE.
+					</span>
+					<span className="leading-7">
+						THIS PROVISION SHALL SURVIVE THE TERMINATION, CESSATION, OR
+						EXPIRATION OF THE USER’S RELATIONSHIP WITH THE COMPANY AND SHALL
+						REMAIN ENFORCEABLE TO THE MAXIMUM EXTENT PERMISSIBLE UNDER
+						APPLICABLE LAW.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						3. TRADEMARKS AND BRAND IDENTITY
+					</span>
+
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							IN THE EVENT THAT ANY THIRD-PARTY ASSERTS A CLAIM, ALLEGATION, OR
+							LEGAL ACTION AGAINST THE COMPANY ARISING FROM USER ACTIONS OR
+							OMISSIONS, THE USER SHALL, UPON WRITTEN NOTICE FROM THE COMPANY:
+						</span>
+						<ul className="list-decimal list-inside ml-4 flex flex-col gap-4">
+							<li>
+								IMMEDIATELY ASSUME FULL CONTROL OF THE DEFENSE OF SUCH CLAIM;
+							</li>
+							<li>
+								RETAIN LEGAL COUNSEL ACCEPTABLE TO THE COMPANY, WHOSE FEES AND
+								EXPENSES SHALL BE BORNE EXCLUSIVELY BY THE USER;
+							</li>
+							<li>
+								REFRAIN FROM ENTERING INTO ANY SETTLEMENT, STIPULATION, OR
+								RESOLUTION WITHOUT THE PRIOR WRITTEN CONSENT OF THE COMPANY.
+							</li>
+						</ul>
+						<span>
+							IN THE EVENT THAT THE USER FAILS TO TIMELY UNDERTAKE THE DEFENSE,
+							THE COMPANY RESERVES THE UNEQUIVOCAL RIGHT TO ASSUME DIRECT
+							CONTROL AND SEEK FULL REIMBURSEMENT FROM THE USER FOR ALL
+							ASSOCIATED COSTS AND DAMAGES.
+						</span>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						4. NO LIMITATION ON COMPANY REMEDIES
+					</span>
+					<span className="leading-7">
+            {`THE COMPANY'S EXERCISE OF INDEMNIFICATION RIGHTS SHALL NOT PRECLUDE,
+						RESTRICT, OR OTHERWISE LIMIT ITS ABILITY TO PURSUE ADDITIONAL
+						REMEDIES, INCLUDING BUT NOT LIMITED TO SEEKING INJUNCTIVE RELIEF,
+						MONETARY DAMAGES, OR OTHER LEGAL RECOURSE AS PERMISSIBLE UNDER LAW.`}
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						5. MODIFICATIONS AND REVISIONS
+					</span>
+					<span className="leading-7">
+						THE COMPANY RESERVES THE UNEQUIVOCAL RIGHT TO AMEND, MODIFY, OR
+						TERMINATE THIS INDEMNIFICATION POLICY AT ANY TIME WITHOUT PRIOR
+						NOTICE. CONTINUED USE OF THE SERVICES SHALL CONSTITUTE IRREVOCABLE
+						ACCEPTANCE OF ANY REVISED TERMS.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						6. CONTACT INFORMATION
+					</span>
+
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							FOR ANY QUESTIONS OR CONCERNS REGARDING THIS POLICY, USERS MAY
+							CONTACT:
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li className="font-bold">Company Name: COMPANY NAME</li>
+							<li className="font-bold">Registered Address: COMPANY ADDRESS</li>
+							<li className="font-bold">
+								Company Registration Number: COMPANY NUMBER
+							</li>
+							<li>
+								<span className="font-bold">Email: </span>
+								support@medexcuse.com
+							</li>
+							<li>
+								<span className="font-bold">Website: </span>
+								https://medexcuse.com
+							</li>
+						</ul>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+
+				<span className="leading-7">
+					BY CONTINUING TO UTILIZE THE SERVICES PROVIDED BY MED EXCUSE, YOU
+					HEREBY AFFIRM THAT YOU HAVE READ, UNDERSTOOD, AND IRREVOCABLY AGREED
+					TO THE TERMS SET FORTH IN THIS INDEMNIFICATION POLICY IN ITS ENTIRETY.
+				</span>
+			</div>
+		</div>
+	);
+}

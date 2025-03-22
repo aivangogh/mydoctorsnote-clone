960f1830fc7668e08944b3611e0ec7153a62c20e
Ivan Gemota <ivangemota23@gmail.com>
2025-03-22 13:15:25 +0800

    chore: added jurisdiction policy page

---
+++ src/app/pages/jurisdiction-policy/page.tsx
@@ -0,0 +1,144 @@
+export default function JurisdictionPolicy() {
+	return (
+		<div className="flex flex-col gap-10">
+			<h1 className="text-5xl">Jurisdiction Policy</h1>
+
+			<div className="flex flex-col gap-6 text-gray-600 text-sm">
+				<div className="flex flex-col gap-4">
+					<span className="font-bold">Last Updated: February 4, 2025</span>
+					<span className="font-regular leading-7">
+						<span className="font-bold">IMPORTANT NOTICE:</span> BY ACCESSING,
+						UTILIZING, OR ENGAGING WITH THE SERVICES OF MED EXCUSE, USERS
+						IRREVOCABLY CONSENT TO THE GOVERNING LAW AND JURISDICTION CLAUSES
+						HEREIN. ANY DISAGREEMENT NECESSITATES IMMEDIATE DISCONTINUATION OF
+						SERVICE USAGE.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						1. APPLICABLE LAW
+					</span>
+					<span className="leading-7">
+						{`ALL MATTERS ARISING OUT OF OR RELATING TO THE USER'S INTERACTION
+						WITH, UTILIZATION OF, OR DEPENDENCE UPON THE SERVICES RENDERED BY
+						MED EXCUSE SHALL BE EXCLUSIVELY GOVERNED, INTERPRETED, AND ENFORCED
+						IN ACCORDANCE WITH THE STATUTORY, REGULATORY, AND COMMON LAW
+						FRAMEWORKS OF THE HONG KONG SPECIAL ADMINISTRATIVE REGION ("HKSAR"),
+						EXCLUDING ITS CONFLICT OF LAW PRINCIPLES. USERS HEREBY IRREVOCABLY
+						WAIVE ANY CLAIM, DEFENSE, OR OBJECTION PERTAINING TO THE APPLICATION
+						OF ANY OTHER JURISDICTION'S LAWS.`}
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						2. EXCLUSIVE JURISDICTION & VENUE
+					</span>
+					<span className="leading-7">
+						ANY CLAIM, DISPUTE, CONTROVERSY, OR CAUSE OF ACTION ARISING FROM,
+						RELATED TO, OR CONNECTED WITH THE SERVICES SHALL BE RESOLVED SOLELY
+						AND EXCLUSIVELY IN THE COURTS OF HONG KONG. USERS HEREBY WAIVE ANY
+						OBJECTION TO PERSONAL JURISDICTION, IMPROPER VENUE, OR FORUM NON
+						CONVENIENS, AND EXPRESSLY CONSENT TO THE ADJUDICATION OF DISPUTES IN
+						THE DESIGNATED JUDICIAL FORUM.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						3. WAIVER OF CLASS ACTION & JURY TRIAL
+					</span>
+					<span className="leading-7">
+						TO THE MAXIMUM EXTENT PERMISSIBLE UNDER LAW, USERS AGREE THAT ANY
+						AND ALL DISPUTES SHALL BE RESOLVED INDIVIDUALLY, ON A CASE-BY-CASE
+						BASIS, AND EXPRESSLY WAIVE ANY RIGHT TO PARTICIPATE IN OR INITIATE A
+						CLASS ACTION, COLLECTIVE PROCEEDING, OR REPRESENTATIVE CLAIM.
+						FURTHERMORE, USERS IRREVOCABLY WAIVE ANY RIGHT TO A TRIAL BY JURY,
+						STIPULATING THAT ALL DISPUTES SHALL BE RESOLVED SOLELY THROUGH BENCH
+						TRIAL ADJUDICATION.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						4. NO EXTRATERRITORIAL APPLICATION
+					</span>
+					<span className="leading-7">
+						UNDER NO CIRCUMSTANCES SHALL ANY USER, ENTITY, OR THIRD PARTY
+						ATTEMPT TO INVOKE OR APPLY THE STATUTES, LAWS, OR REGULATORY
+						FRAMEWORKS OF ANY OTHER JURISDICTION TO GOVERN OR REGULATE THE
+						SERVICES PROVIDED BY MED EXCUSE. ANY ATTEMPT TO ENFORCE FOREIGN
+						LAWS, REGULATIONS, OR JUDGMENTS SHALL BE CONSIDERED NULL, VOID, AND
+						LEGALLY UNENFORCEABLE.
+					</span>
+
+					<hr className="border-gray-200 my-10" />
+					<div className="flex flex-col gap-4">
+						<span className="font-bold text-black text-[16px]">
+							5. NON-NEGOTIABILITY & SEVERABILITY
+						</span>
+						<span className="leading-7">
+							THE TERMS SET FORTH HEREIN CONSTITUTE A FUNDAMENTAL COMPONENT OF
+							THE LEGAL RELATIONSHIP BETWEEN THE USER AND MED EXCUSE. SHOULD ANY
+							PROVISION OF THIS POLICY BE DETERMINED TO BE INVALID,
+							UNENFORCEABLE, OR CONTRARY TO LAW, SUCH DETERMINATION SHALL NOT
+							AFFECT THE REMAINING PROVISIONS, WHICH SHALL REMAIN IN FULL FORCE
+							AND EFFECT.
+						</span>
+					</div>
+					<hr className="border-gray-200 my-10" />
+					<div className="flex flex-col gap-4">
+						<span className="font-bold text-black text-[16px]">
+							6. MODIFICATIONS AND REVISIONS
+						</span>
+						<span className="leading-7">
+							THE COMPANY RESERVES THE UNEQUIVOCAL RIGHT TO AMEND, MODIFY, OR
+							TERMINATE THIS GOVERNING LAW & JURISDICTION POLICY AT ANY TIME
+							WITHOUT PRIOR NOTICE. CONTINUED USE OF THE SERVICES SHALL
+							CONSTITUTE IRREVOCABLE ACCEPTANCE OF ANY REVISED TERMS.
+						</span>
+					</div>
+					<hr className="border-gray-200 my-10" />
+					<div className="flex flex-col gap-4">
+						<span className="font-bold text-black text-[16px]">
+							7. CONTACT INFORMATION
+						</span>
+
+						<span className="flex flex-col gap-6 font-regular leading-7">
+							<span>
+								FOR ANY QUESTIONS OR CONCERNS REGARDING THIS POLICY, USERS MAY
+								CONTACT:
+							</span>
+							<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+								<li className="font-bold">Company Name: COMPANY NAME</li>
+								<li className="font-bold">
+									Registered Address: COMPANY ADDRESS
+								</li>
+								<li className="font-bold">
+									Company Registration Number: COMPANY NUMBER
+								</li>
+								<li>
+									<span className="font-bold">Email: </span>
+									support@medexcuse.com
+								</li>
+								<li>
+									<span className="font-bold">Website: </span>
+									https://medexcuse.com
+								</li>
+							</ul>
+						</span>
+					</div>
+					<hr className="border-gray-200 my-10" />
+
+					<span className="leading-7">
+						BY CONTINUING TO UTILIZE THE SERVICES PROVIDED BY MED EXCUSE, YOU
+						HEREBY AFFIRM THAT YOU HAVE READ, UNDERSTOOD, AND IRREVOCABLY AGREED
+						TO THE TERMS SET FORTH IN THIS GOVERNING LAW & JURISDICTION POLICY
+						IN ITS ENTIRETY.
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+}

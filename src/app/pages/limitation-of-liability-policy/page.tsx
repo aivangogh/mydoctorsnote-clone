196913958eb042c0e28223a099e449ad6ea5fa34
Ivan Gemota <ivangemota23@gmail.com>
2025-03-22 13:22:18 +0800

    chore: added limitation of liability policy page

---
+++ src/app/pages/limitation-of-liability-policy/page.tsx
@@ -0,0 +1,154 @@
+export default function LimitationOfLiabilityPolicy() {
+	return (
+		<div className="flex flex-col gap-10">
+			<h1 className="text-5xl">Limitation of Liability Policy</h1>
+
+			<div className="flex flex-col gap-6 text-gray-600 text-sm">
+				<div className="flex flex-col gap-4">
+					<span className="font-bold">Last Updated: February 4, 2025</span>
+					<span className="font-regular leading-7">
+						<span className="font-bold">IMPORTANT NOTICE:</span> BY ENGAGING
+						WITH THE SERVICES OF MED EXCUSE, YOU UNEQUIVOCALLY ACKNOWLEDGE AND
+						ACCEPT THE LIMITATIONS OF LIABILITY SET FORTH HEREIN. ANY
+						DISAGREEMENT NECESSITATES IMMEDIATE CESSATION OF SERVICE
+						UTILIZATION.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						1. ABSOLUTE DISCLAIMER OF LIABILITY
+					</span>
+					<span className="leading-7">
+						<span className="font-bold">COMPANY NAME</span>
+						{` (hereinafter referred to as "Company," "We," "Us," "Our"), a duly registered entity under the jurisdiction of Hong Kong, HEREBY DISCLAIMS, TO THE FULLEST EXTENT PERMISSIBLE UNDER APPLICABLE LAW, ANY AND ALL LIABILITY ARISING FROM, CONNECTED TO, OR RELATED TO THE UTILIZATION, MISUSE, OR INTERPRETATION OF ANY CONTENT, SERVICES, OR DIGITAL PRODUCTS PROVIDED THROUGH MED EXCUSE (HEREINAFTER REFERRED TO AS "WEBSITE").`}
+					</span>
+					<span className="leading-7">
+						UNDER NO CIRCUMSTANCES SHALL THE COMPANY, ITS OFFICERS, DIRECTORS,
+						EMPLOYEES, CONTRACTORS, AFFILIATES, OR LICENSORS BE HELD LIABLE FOR
+						ANY DIRECT, INDIRECT, INCIDENTAL, CONSEQUENTIAL, SPECIAL, EXEMPLARY,
+						OR PUNITIVE DAMAGES, INCLUDING BUT NOT LIMITED TO LOSS OF PROFITS,
+						DATA, BUSINESS OPPORTUNITY, REPUTATIONAL DAMAGE, OR LEGAL
+						REPERCUSSIONS, REGARDLESS OF THE LEGAL THEORY UNDER WHICH SUCH
+						LIABILITY IS CLAIMED.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						2. NO GUARANTEE OF OUTCOME OR UTILITY
+					</span>
+					<span className="leading-7">
+						THE COMPANY MAKES NO REPRESENTATIONS, WARRANTIES, OR GUARANTEES,
+						EXPRESS OR IMPLIED, REGARDING THE FITNESS, SUITABILITY, ACCURACY, OR
+						ACCEPTANCE OF ITS SERVICES OR DOCUMENT TEMPLATES BY ANY THIRD PARTY,
+						INCLUDING BUT NOT LIMITED TO EMPLOYERS, INSURANCE PROVIDERS,
+						REGULATORY AGENCIES, MEDICO-LEGAL AUTHORITIES, OR JUDICIAL BODIES.
+						USERS BEAR THE FULL RESPONSIBILITY OF VERIFYING THE APPLICABILITY OF
+						ANY TEMPLATE DOCUMENT TO THEIR SPECIFIC CIRCUMSTANCES.
+					</span>
+					<span className="leading-7">
+						ANY DECISION TO RELY UPON THE CONTENT OR SERVICES PROVIDED BY THE
+						COMPANY IS MADE AT THE USER’S SOLE VOLITION AND RISK.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						3. FORCE MAJEURE
+					</span>
+					<span className="leading-7">
+						TO THE MAXIMUM EXTENT PERMISSIBLE UNDER LAW, USERS AGREE THAT ANY
+						AND ALL DISPUTES SHALL BE RESOLVED INDIVIDUALLY, ON A CASE-BY-CASE
+						BASIS, AND EXPRESSLY WAIVE ANY RIGHT TO PARTICIPATE IN OR INITIATE A
+						CLASS ACTION, COLLECTIVE PROCEEDING, OR REPRESENTATIVE CLAIM.
+						FURTHERMORE, USERS IRREVOCABLY WAIVE ANY RIGHT TO A TRIAL BY JURY,
+						STIPULATING THAT ALL DISPUTES SHALL BE RESOLVED SOLELY THROUGH BENCH
+						TRIAL ADJUDICATION.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						4. THIRD-PARTY INTERACTIONS
+					</span>
+					<span className="leading-7">
+						THE COMPANY DOES NOT ENDORSE, MONITOR, OR ASSUME RESPONSIBILITY FOR
+						ANY THIRD-PARTY SERVICES, LINKS, OR RESOURCES THAT USERS MAY ACCESS
+						THROUGH THE WEBSITE. USERS INTERACT WITH EXTERNAL ENTITIES SOLELY AT
+						THEIR OWN RISK AND SUBJECT TO THE TERMS OF SUCH THIRD PARTIES.
+					</span>
+
+					<hr className="border-gray-200 my-10" />
+					<div className="flex flex-col gap-4">
+						<span className="font-bold text-black text-[16px]">
+							5. LIMITATION OF LIABILITY TO MONETARY AMOUNT
+						</span>
+						<span className="leading-7">
+              {`IN JURISDICTIONS WHERE LIABILITY LIMITATIONS MAY BE ENFORCED,
+							USERS EXPRESSLY AGREE THAT THE COMPANY’S TOTAL AGGREGATE
+							LIABILITY, IF ANY, SHALL NOT EXCEED THE AMOUNT PAID BY THE USER
+							FOR SERVICES RENDERED WITHIN THE IMMEDIATELY PRECEDING THREE (3)
+							MONTHS OF THE CLAIM'S OCCURRENCE.`}
+						</span>
+						<span className="leading-7">
+							IN INSTANCES WHERE LOCAL LAWS RESTRICT LIABILITY EXCLUSIONS, USERS
+							MAY HAVE ADDITIONAL RIGHTS; HOWEVER, SUCH RIGHTS SHALL BE LIMITED
+							TO THE MAXIMUM EXTENT PERMISSIBLE UNDER APPLICABLE LAW.
+						</span>
+					</div>
+					<hr className="border-gray-200 my-10" />
+					<div className="flex flex-col gap-4">
+						<span className="font-bold text-black text-[16px]">
+							6. MODIFICATIONS AND REVISIONS
+						</span>
+						<span className="leading-7">
+							THE COMPANY RESERVES THE UNEQUIVOCAL RIGHT TO AMEND, MODIFY, OR
+							TERMINATE THIS LIMITATION OF LIABILITY POLICY AT ANY TIME WITHOUT
+							PRIOR NOTICE. CONTINUED USE OF THE SERVICES SHALL CONSTITUTE
+							IRREVOCABLE ACCEPTANCE OF ANY REVISED TERMS.
+						</span>
+					</div>
+					<hr className="border-gray-200 my-10" />
+					<div className="flex flex-col gap-4">
+						<span className="font-bold text-black text-[16px]">
+							7. CONTACT INFORMATION
+						</span>
+
+						<span className="flex flex-col gap-6 font-regular leading-7">
+							<span>
+								FOR ANY QUESTIONS OR CONCERNS REGARDING THIS POLICY, USERS MAY
+								CONTACT:
+							</span>
+							<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+								<li className="font-bold">Company Name: COMPANY NAME</li>
+								<li className="font-bold">
+									Registered Address: COMPANY ADDRESS
+								</li>
+								<li className="font-bold">
+									Company Registration Number: COMPANY NUMBER
+								</li>
+								<li>
+									<span className="font-bold">Email: </span>
+									support@medexcuse.com
+								</li>
+								<li>
+									<span className="font-bold">Website: </span>
+									https://medexcuse.com
+								</li>
+							</ul>
+						</span>
+					</div>
+					<hr className="border-gray-200 my-10" />
+
+					<span className="leading-7">
+						BY CONTINUING TO UTILIZE THE SERVICES PROVIDED BY MED EXCUSE, YOU
+						HEREBY AFFIRM THAT YOU HAVE READ, UNDERSTOOD, AND IRREVOCABLY AGREED
+						TO THE TERMS SET FORTH IN THIS LIMITATION OF LIABILITY POLICY IN ITS
+						ENTIRETY.
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+}

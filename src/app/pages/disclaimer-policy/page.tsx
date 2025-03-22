9acc39b0e3483cecdbfb763f636fb19dac1a5066
Ivan Gemota <ivangemota23@gmail.com>
2025-03-22 13:00:41 +0800

    chore: added disclaimer policy page

---
+++ src/app/pages/disclaimer-policy/page.tsx
@@ -0,0 +1,253 @@
+export default function DisclaimerPolicy() {
+	return (
+		<div className="flex flex-col gap-10">
+			<h1 className="text-5xl">Disclaimer Policy</h1>
+
+			<div className="flex flex-col gap-6 text-gray-600 text-sm">
+				<div className="flex flex-col gap-4">
+					<span className="font-bold">Last Updated: February 4, 2025</span>
+					<span className="font-regular leading-7">
+						<span className="font-bold">IMPORTANT NOTICE:</span> BY ACCESSING,
+						UTILIZING, OR ENGAGING WITH THE SERVICES OF MED EXCUSE, YOU
+						IRREVOCABLY ASSENT TO THE DISCLAIMERS AND LIMITATIONS OF LIABILITY
+						HEREIN. ANY DISAGREEMENT WITH THESE TERMS NECESSITATES IMMEDIATE
+						CESSATION OF SERVICE USAGE.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						1. GENERAL DISCLAIMER
+					</span>
+					<span className="font-regular leading-7">
+						<span className="font-bold">COMPANY NAME</span>
+						{` (hereinafter referred to as "Company," "We," "Us," "Our"), a legally registered entity under the jurisdiction of Hong Kong, exclusively facilitates the automated generation of template-based documents through MedExcuse.com (hereinafter referred to as "Website").`}
+					</span>
+					<span className="leading-7">
+            {`UNDER NO CIRCUMSTANCES SHALL OUR SERVICES BE CONSTRUED AS
+						CONFERRING, ENDORSING, OR EMULATING LEGITIMATE CLINICO-THERAPEUTIC
+						ADVICE, DIAGNOSTIC SUBSTANTIATION, OR CERTIFICATION. ANY PURPORTED
+						APPLICATION OR UTILIZATION OF THE DOCUMENTS RENDERED BY OUR PLATFORM
+						IN A MANNER SUGGESTING CLINICO-THERAPEUTIC VERACITY OR PROFESSIONAL
+						VALIDATION IS HEREBY CATEGORICALLY REFUTED AND EXPRESSLY PROHIBITED.
+						THE COMPANY BEARS NO RESPONSIBILITY FOR USERS' INTERPRETATIONS,
+						MISCONCEPTIONS, OR EXPECTATIONS OF LEGITIMACY OR RECOGNITION
+						ASSOCIATED WITH THE OUTPUT GENERATED.`}
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						2. NO GUARANTEE OF ACCEPTANCE OR LEGAL VALIDITY
+					</span>
+					<span className="leading-7">
+						THE COMPANY UNEQUIVOCALLY DISCLAIMS ANY AND ALL LIABILITY ARISING
+						FROM THE FAILURE, REJECTION, OR NON-RECOGNITION OF ITS GENERATED
+						DOCUMENTS BY ANY EMPLOYER, CLINICO-THERAPEUTIC ESTABLISHMENT,
+						GOVERNMENTAL BODY, OR LEGAL INSTITUTION. USERS HEREBY RECOGNIZE THE
+						INHERENT RISK AND ABSENCE OF CERTAINTY REGARDING ACCEPTANCE AND
+						SHALL NOT ATTRIBUTE ANY CONSEQUENTIAL DAMAGES, DETRIMENTAL OUTCOMES,
+						OR PROFESSIONAL REPERCUSSIONS TO THE COMPANY.
+					</span>
+					<span className="leading-7">
+            {`OUR SERVICES PROVIDE ONLY UNSANCTIONED, NON-CERTIFIED, AND WHOLLY
+						NON-BINDING TEMPLATE-BASED OUTPUTS DERIVED EXCLUSIVELY FROM
+						USER-PROVIDED DATA, WITHOUT AMENDMENT, VERIFICATION, OR
+						LEGITIMATION. ANY RELIANCE ON SUCH MATERIALS IS UNDERTAKEN SOLELY AT
+						THE USER'S VOLITION AND PERIL, AND THE COMPANY SHALL NOT BE IMPUGNED
+						FOR ANY ADVERSE CONSEQUENCES RESULTING THEREFROM.`}
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						3. NO LIABILITY FOR USER ACTIONS OR MISUSE
+					</span>
+					<span className="leading-7">
+						THE COMPANY STRICTLY DISAVOWS ALL FORMS OF FRAUDULENT, MISLEADING,
+						OR UNETHICAL APPLICATIONS OF ITS SERVICES. USERS EXPLICITLY
+						ACKNOWLEDGE THAT ANY MISREPRESENTATION, FORGERY, OR UNAUTHORIZED
+						USAGE OF THE GENERATED DOCUMENTS IS NOT ONLY STRICTLY PROHIBITED BUT
+						MAY ALSO CARRY LEGAL CONSEQUENCES FOR WHICH THE COMPANY DISCLAIMS
+						ALL RESPONSIBILITY.
+					</span>
+					<span className="leading-7">
+						UNDER NO EVENTUALITY SHALL THE COMPANY BE IMPUTED FOR USERS’ LEGAL
+						LIABILITY, OCCUPATIONAL SANCTIONS, OR JUDICIAL IMPLICATIONS ARISING
+						FROM THE DISSEMINATION, UTILIZATION, OR ATTEMPTED LEGITIMATION OF
+						THE TEMPLATES PROVIDED. USERS SHALL BE SINGULARLY ACCOUNTABLE FOR
+						ENSURING THAT ANY DOCUMENT GENERATED IS EMPLOYED IN STRICT
+						ACCORDANCE WITH APPLICABLE LAWS AND REGULATIONS.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						4. LIMITATION OF LIABILITY
+					</span>
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							TO THE FULLEST EXTENT PERMISSIBLE BY LAW, THE COMPANY, ITS
+							EXECUTIVES, EMPLOYEES, PARTNERS, AND AFFILIATES SHALL BE HELD
+							INDEFENSIBLE AGAINST ANY LIABILITY FOR:
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li>
+								PECUNIARY OR ECONOMIC LOSSES, INCLUDING BUT NOT LIMITED TO
+								EMPLOYMENT TERMINATION, FINANCIAL REPRIMANDS, OR LOST
+								OPPORTUNITIES;
+							</li>
+							<li>
+								DISCIPLINARY, LEGAL, OR JURISPRUDENTIAL PROCEEDINGS ARISING FROM
+								DOCUMENT USAGE;
+							</li>
+							<li>
+								ADVERSE ACTIONS TAKEN BY THIRD PARTIES IN RESPONSE TO THE
+								PRESENTATION OF GENERATED DOCUMENTS;
+							</li>
+							<li>
+								ANY CONSEQUENTIAL, INCIDENTAL, OR EXEMPLARY DAMAGES IRRESPECTIVE
+								OF FORESEEABILITY.
+							</li>
+						</ul>
+						<span>
+							IN NO INSTANCE SHALL THE CUMULATIVE LIABILITY OF THE COMPANY
+							EXCEED THE MONETARY SUM REMITTED BY THE USER FOR THE SERVICE IN
+							QUESTION. ALL LIABILITY DISCLAIMERS HEREIN ARE FULLY APPLICABLE
+							REGARDLESS OF LEGAL THEORY, TORTIOUS CLAIM, OR ALLEGED NEGLIGENCE.
+						</span>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						5. INDEMNIFICATION
+					</span>
+
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							TO THE FULLEST EXTENT PERMISSIBLE BY LAW, THE COMPANY, ITS
+							EXECUTIVES, EMPLOYEES, PARTNERS, AND AFFILIATES SHALL BE HELD
+							INDEFENSIBLE AGAINST ANY LIABILITY FOR:
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li>
+								ANY FRAUDULENT, ERRONEOUS, OR UNAUTHORIZED USE OF THE GENERATED
+								DOCUMENTS;
+							</li>
+							<li>
+								ANY ATTEMPTED ASSERTION OF LEGITIMACY OR ACCREDITATION OF
+								DOCUMENTS WHERE NONE EXISTS;
+							</li>
+							<li>
+                {`ANY THIRD-PARTY RELIANCE OR MISCONCEPTION REGARDING THE
+								DOCUMENTS GENERATED BY THE COMPANY'S PLATFORM.undertake
+								reasonable measures to ensure their satisfaction.`}
+							</li>
+						</ul>
+						<span>
+							THE COMPANY SHALL BE HELD WHOLLY IMMUNE FROM ALL LEGAL
+							REPERCUSSIONS, SANCTIONS, OR CLAIMS ARISING FROM USERS’ ACTIONS,
+							MISINTERPRETATIONS, OR NONCOMPLIANT UTILIZATION OF SERVICES
+							RENDERED.
+						</span>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						6. MODIFICATIONS TO DISCLAIMER POLICY
+					</span>
+					<span className="leading-7">
+						THE COMPANY RESERVES THE UNEQUIVOCAL RIGHT TO AMEND, RESCIND, OR
+						REVISE THIS DISCLAIMER POLICY WITHOUT PRIOR NOTICE OR CONSENT. USERS
+						SHALL BE SOLELY RESPONSIBLE FOR PERIODICALLY REVIEWING THE POLICY TO
+						ENSURE CONTINUED COMPLIANCE.
+					</span>
+
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							By assenting to these Terms of Service, you irrevocably commit to
+							indemnify, defend, and hold harmless the Company, its affiliates,
+							executives, representatives, and agents against any liabilities,
+							claims, damages, or expenses arising from:
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li>Any deceptive or illicit employment of our templates.</li>
+							<li>
+								Any litigations, inquiries, or disputes initiated by employers,
+								insurers, governmental bodies, or third-party entities regarding
+								the authenticity of our documents.
+							</li>
+							<li>
+								Any and all financial penalties, legal fees, or damages incurred
+								as a consequence of your actions.
+							</li>
+						</ul>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						7. GOVERNING LAW AND DISPUTE RESOLUTION
+					</span>
+					<span className="leading-7">
+						These Terms of Service shall be governed, interpreted, and enforced
+						exclusively under the laws of Hong Kong, irrespective of
+						conflict-of-law principles.
+					</span>
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li>
+								Any disputes shall first be subject to good-faith negotiations.
+							</li>
+							<li>
+								Failing amicable resolution, disputes shall be conclusively
+								adjudicated via binding arbitration in Hong Kong.
+							</li>
+							<li>
+								Users explicitly forfeit the right to initiate or participate in
+								any class action litigation.
+							</li>
+						</ul>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						7. CONTACT INFORMATION
+					</span>
+
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							FOR INQUIRIES OR DISPUTES PERTAINING TO THIS DISCLAIMER POLICY,
+							USERS MAY CONTACT:
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li className="font-bold">Company Name: COMPANY NAME</li>
+							<li className="font-bold">Registered Address: COMPANY ADDRESS</li>
+							<li className="font-bold">
+								Company Registration Number: COMPANY NUMBER
+							</li>
+							<li>
+								<span className="font-bold">Email: </span>
+								support@medexcuse.com
+							</li>
+							<li>
+								<span className="font-bold">Website: </span>
+								https://medexcuse.com
+							</li>
+						</ul>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+
+				<span className="leading-7">
+					BY ENGAGING WITH OUR SERVICES, YOU HEREBY WARRANT THAT YOU HAVE READ,
+					UNDERSTOOD, AND IRREVOCABLY ACCEPTED THE TERMS OF THIS DISCLAIMER
+					POLICY IN ITS ENTIRETY.
+				</span>
+			</div>
+		</div>
+	);
+}

4f02dba5ecf43b777949a99498ca436e70eaab98
Ivan Gemota <ivangemota23@gmail.com>
2025-03-22 12:45:17 +0800

    chore: added privacy policy page

---
+++ src/app/pages/privacy-policy/page.tsx
@@ -0,0 +1,291 @@
+export default function PrivacyPolicy() {
+	return (
+		<div className="flex flex-col gap-10">
+			<h1 className="text-5xl">Privacy Policy</h1>
+
+			<div className="flex flex-col gap-6 text-gray-600 text-sm">
+				<div className="flex flex-col gap-4">
+					<span className="font-bold">Last Updated: February 4, 2025</span>
+					<span className="font-regular leading-7">
+						<span className="font-bold">IMPORTANT NOTICE:</span> BY ACCESSING,
+						UTILIZING, OR ENGAGING WITH THIS DIGITAL PLATFORM, YOU IRREVOCABLY
+						ASSENT TO THE DATA COLLECTION, UTILIZATION, AND DISCLOSURE PRACTICES
+						ENUMERATED HEREIN. NON-COMPLIANCE NECESSITATES IMMEDIATE CESSATION
+						OF ALL USAGE AND RESTRICTION FROM FUTURE ENGAGEMENTS.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						1. INTRODUCTION
+					</span>
+					<span className="font-regular leading-7">
+						<span className="font-bold">COMPANY NAME</span>
+						{`, a duly incorporated entity within the jurisdiction of Hong Kong (hereinafter referred to as "Company," "We," "Us," "Our"), exclusively operates the domain MedExcuse.com (hereinafter referred to as "Website"). This Privacy Policy governs all mechanisms by which the Company gathers, retains, processes, and disseminates user-provided data in strict adherence to all applicable statutes governing digital privacy and data security.`}
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						2. INFORMATION WE COLLECT
+					</span>
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							The Company, through user interaction with the Website,
+							accumulates and processes the following categorical data:
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li>
+								<span className="font-bold">
+									Personally Identifiable Information (PII):
+								</span>{" "}
+								Including but not limited to names, email addresses, IP
+								addresses, transactional records, and digital identifiers.
+							</li>
+							<li>
+								<span className="font-bold">Device and Analytical Data:</span>{" "}
+								Encompassing browser type, device specifications, session
+								durations, access timestamps, and navigation patterns.{" "}
+							</li>
+							<li>
+								<span className="font-bold">Transactional Information:</span>{" "}
+								Transactional Information:
+							</li>
+
+							<li>
+								<span className="font-bold">User-Supplied Content:</span> Data
+								inputted by the user into form fields for document generation.
+							</li>
+						</ul>
+						<span>
+							All data collection is executed through encrypted channels and
+							safeguarded under industry-standard security protocols.
+						</span>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						3. PURPOSE OF DATA COLLECTION
+					</span>
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>We deploy collected data exclusively for:</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li>
+								Facilitating automated document generation per user
+								specifications.
+							</li>
+							<li>
+								Processing financial transactions and furnishing electronic
+								receipts.
+							</li>
+							<li>
+								Enforcing compliance with our Terms of Service and fraud
+								prevention.
+							</li>
+							<li>
+								Enhancing website functionality, operational security, and user
+								experience optimization.
+							</li>
+							<li>
+								Conducting legal and regulatory compliance evaluations as
+								necessitated by governing authorities.
+							</li>
+						</ul>
+						<span>
+							Conducting legal and regulatory compliance evaluations as
+							necessitated by governing authorities.
+						</span>
+					</span>
+				</div>{" "}
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						4. DATA RETENTION & STORAGE
+					</span>
+					<span className="leading-7">
+						User-provided data shall be retained strictly for the duration
+						necessary to fulfill the stated purposes of collection. Following
+						completion of service or user request for deletion, all personally
+						identifiable data shall be permanently expunged, except where
+						legally mandated retention periods apply.
+					</span>
+					<span className="leading-7">
+						Data is stored on secure, access-restricted servers and subject to
+						routine integrity audits to ensure compliance with international
+						data protection regulations.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						5. DISCLOSURE OF INFORMATION
+					</span>
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							User data shall NOT be disclosed, sold, leased, or otherwise
+							transferred to unauthorized third parties except under the
+							following circumstances:
+						</span>
+						<ul className="list-decimal list-inside ml-4 flex flex-col gap-4">
+							<li>
+								<span className="font-bold">Legal Obligations:</span> Where
+								mandated by judicial orders, statutory obligations, or
+								government directives, including fraud investigations or
+								compliance audits.{" "}
+							</li>
+							<li>
+								<span className="font-bold">Transactional Necessity:</span>{" "}
+								Disclosure to payment processors, hosting services, or technical
+								infrastructure providers strictly for operational facilitation.
+							</li>
+							<li>
+								<span className="font-bold">Fraud Prevention & Security:</span>{" "}
+								In instances of suspected fraudulent activity, abuse, or
+								unauthorized usage of our platform, data may be shared with law
+								enforcement or cybersecurity entities.
+							</li>
+							<li>
+								<span className="font-bold">Approved Refund Disbursement:</span>{" "}
+								If approved, the refund shall be issued via the original payment
+								method within{" "}
+								<span className="font-bold">ten (10) business days</span>,
+								subject to processing timelines imposed by financial
+								institutions.
+							</li>
+						</ul>
+						<span>
+							Failure to comply with the aforementioned procedure may result in
+							automatic denial of refund claims.
+						</span>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						6. COOKIES & TRACKING TECHNOLOGIES
+					</span>
+					<span className="leading-7">
+						The Website employs cookies, log files, and analytical tracking
+						technologies to monitor session behaviors, enhance functionality,
+						and ensure secure authentication. Users may configure browser
+						settings to restrict or disable cookies; however, doing so may
+						impair the functionality of certain Website features.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						7. USER RIGHTS & DATA CONTROL
+					</span>
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							Users domiciled in the United States or other applicable
+							jurisdictions retain the following rights regarding personal data:
+						</span>
+						<ul className="list-decimal list-inside ml-4 flex flex-col gap-4">
+							<li>
+								<span className="font-bold">Access & Portability:</span> Request
+								access to stored data and obtain copies in a structured format.
+							</li>
+							<li>
+								<span className="font-bold">Rectification:</span> Modify or
+								update any erroneous or outdated information.
+							</li>
+							<li>
+								<span className="font-bold">Erasure & Deletion:</span> Demand
+								deletion of personally identifiable data, subject to legal
+								retention requirements.
+							</li>
+							<li>
+								<span className="font-bold">Opt-Out:</span> Revoke consent for
+								non-essential data collection and processing.{" "}
+							</li>
+						</ul>
+						<span>
+							All requests shall be processed within a reasonable timeframe upon
+							verification of the user’s identity.{" "}
+						</span>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						8. DATA SECURITY MEASURES
+					</span>
+					<span className="leading-7">
+						We employ rigorous encryption standards, firewall protections, and
+						continuous intrusion detection monitoring to safeguard user data
+						from unauthorized access, breach, or misuse. Despite our efforts, no
+						digital transmission is infallible; therefore, we disclaim liability
+						for data interception or unauthorized access resulting from cyber
+						threats beyond our control.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						9. THIRD-PARTY LINKS & EXTERNAL INTEGRATIONS
+					</span>
+					<span className="leading-7">
+						Our Website may contain hyperlinks directing users to external
+						domains operated by third parties. We assume no responsibility for
+						the privacy policies, data security measures, or content accuracy of
+						such third-party sites.
+					</span>
+					<span className="leading-7">
+						Users navigating away from our Website acknowledge that their
+						engagement with external platforms is governed exclusively by the
+						respective entities’ policies.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						10. MODIFICATIONS TO PRIVACY POLICY
+					</span>
+					<span className="leading-7">
+						We reserve the unilateral right to modify, amend, or update this
+						Privacy Policy at any time without prior notice. Continued
+						engagement with our services following any modifications constitutes
+						tacit acceptance of the revised terms.
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<div className="flex flex-col gap-4">
+					<span className="font-bold text-black text-[16px]">
+						11. CONTACT INFORMATION
+					</span>
+
+					<span className="flex flex-col gap-6 font-regular leading-7">
+						<span>
+							For inquiries regarding data privacy, requests for information
+							access, or policy clarifications, direct correspondence to:
+						</span>
+						<ul className="list-disc list-inside ml-4 flex flex-col gap-4">
+							<li className="font-bold">Company Name: COMPANY NAME</li>
+							<li className="font-bold">Registered Address: COMPANY ADDRESS</li>
+							<li className="font-bold">
+								Company Registration Number: COMPANY NUMBER
+							</li>
+							<li>
+								<span className="font-bold">Email: </span>
+								support@medexcuse.com
+							</li>
+							<li>
+								<span className="font-bold">Website: </span>
+								https://medexcuse.com
+							</li>
+						</ul>
+					</span>
+				</div>
+				<hr className="border-gray-200 my-10" />
+				<span className="leading-7">
+					By engaging with our services, you affirm that you have read,
+					comprehended, and unconditionally agreed to the foregoing Privacy
+					Policy.
+				</span>
+			</div>
+		</div>
+	);
+}
